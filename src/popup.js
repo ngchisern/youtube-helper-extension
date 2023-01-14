8e11ab64dba5723cce9966aa4a72e3a8c2a4c820
GnohChengYi <chengyignoh@gmail.com>
2023-01-14 17:40:17 +0800

    Open OpenAI correctly

--- src/popup.js
+++ src/popup.js
@@ -1,87 +1,9 @@
 'use strict';
 
 import './popup.css';
-import CHAT_URL from './constants.js';
+import { CHAT_URL } from './constants';
 
 (function () {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  // const counterStorage = {
-  //   get: (cb) => {
-  //     chrome.storage.sync.get(['count'], (result) => {
-  //       cb(result.count);
-  //     });
-  //   },
-  //   set: (value, cb) => {
-  //     chrome.storage.sync.set(
-  //       {
-  //         count: value,
-  //       },
-  //       () => {
-  //         cb();
-  //       }
-  //     );
-  //   },
-  // };
-
-  // function setupCounter(initialValue = 0) {
-  //   document.getElementById('counter').innerHTML = initialValue;
-
-  //   document.getElementById('incrementBtn').addEventListener('click', () => {
-  //     updateCounter({
-  //       type: 'INCREMENT',
-  //     });
-  //   });
-
-  //   document.getElementById('decrementBtn').addEventListener('click', () => {
-  //     updateCounter({
-  //       type: 'DECREMENT',
-  //     });
-  //   });
-  // }
-
-  // function updateCounter({ type }) {
-  //   counterStorage.get((count) => {
-  //     let newCount;
-
-  //     if (type === 'INCREMENT') {
-  //       newCount = count + 1;
-  //     } else if (type === 'DECREMENT') {
-  //       newCount = count - 1;
-  //     } else {
-  //       newCount = count;
-  //     }
-
-  //     counterStorage.set(newCount, () => {
-  //       document.getElementById('counter').innerHTML = newCount;
-
-  //       // Communicate with content script of
-  //       // active tab by sending a message
-  //       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  //         const tab = tabs[0];
-
-  //         chrome.tabs.sendMessage(
-  //           tab.id,
-  //           {
-  //             type: 'COUNT',
-  //             payload: {
-  //               count: newCount,
-  //             },
-  //           },
-  //           (response) => {
-  //             console.log('Current count value passed to contentScript file');
-  //           }
-  //         );
-  //       });
-  //     });
-  //   });
-  // }
-
   // https://developer.chrome.com/docs/extensions/reference/tabs/#get-the-current-tab
   async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
@@ -93,37 +15,38 @@ import CHAT_URL from './constants.js';
   async function checkYouTubeVideo() {
     // Check whether watching YouTube video
     console.log('checking YouTube Video');
-    const queryOptions = { active: true, lastFocusedWindow: true };
     const tab = await getCurrentTab();
     const url = tab.url;
     if (url.startsWith('https://www.youtube.com/watch')) {
       console.log('is watching YouTube video');
       document.getElementById('status').innerText = 'Found YouTube Video'
+      return true;
     } else {
       console.log('not watching YouTube video');
       document.getElementById('status').innerText = 'Cannot find YouTube Video'
+      return false;
     }
   }
 
   async function checkLogin() {
-    console.log('checking login');
     // Check whether already login to OpenAI
+    console.log('checking login');
     var port = chrome.runtime.connect({});
     port.postMessage({ type: 'AUTHENTICATE' });
     port.onMessage.addListener(function (resp) {
       console.log(`Authenticated: ${resp.isAuthenticated}`);
       if (!resp.isAuthenticated) {
-        document.getElementById('button').value = 'Login';
+        console.log(`not authenticated, opening CHAT_URL:${CHAT_URL}`);
+        document.getElementById('button').innerHTML = 'Login';
         document.getElementById('button').addEventListener('click', () => {
-          window.open(CHAT_URL, '_blank');
-          // chrome.tabs.create({ "url": CHAT_URL });
+          window.open(CHAT_URL, '_newtab');
         });
       }
     })
   }
 
   async function check() {
-    await checkYouTubeVideo();
+    const isYouTubeVideo = await checkYouTubeVideo();
     await checkLogin();
     // TODO summarize button
   }
