131f82bfd071e420b4e3d89a671bf9bdb56a759f
ngchisern <ncsern2000@gmail.com>
2023-01-14 15:56:30 +0800

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,21 +1,63 @@
+import { ask, getAccessToken, removeConversation  } from './chat-gpt.js'
+import { QUERY } from './constants.js';
+import { fetchSubtitle } from './save-sub.js';
+
 'use strict';
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
+async function getOpenAIToken() {
+  return getAccessToken()
+}
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+async function summarize(req, port) {
+  let uuid = 'b9cde5a6-1ba5-4b0c-90e9-0a4d5d58c223'
+  let puuid = 'be900e07-b6d2-422b-b666-56a908f85b66'
+  let subtitle = fetchSubtitle(req.url)
+  let query = QUERY.format(subtitle)
+  let token = getOpenAIToken() 
+  resp = ask(uuid, puuid, query, token)
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+  reader = resp.body.pipeThrough(new TextDecoderStream()).getReader()
+  let conversationId = undefined
+
+  while (True) {
+    const {value, done} = await reader.read();
+    if (done) {
+      break;
+    }
+    console.log(value)
+    if (!value.startsWith('data: ')) {
+      continue
+    }
+    data = JSON.parse(value.split("data: ")[1])
+    conversationId = data.conversation_id
+    port.postMessage({type: "SUMMARY", content: data.message?.content?.parts?.[0]});
   }
+  await removeConversation(conversationId, token)
+  port.disconnect()
+}
+
+async function authenticate(port) {
+  try {
+    getOpenAIToken()
+    port.postMessage({ isAuthenticated: true })
+  } catch (err) {
+    port.postMessage({ isAuthenticated: false })
+  } finally {
+    port.disconnect()
+  }
+}
+
+chrome.runtime.onConnect.addListener(function(port) {
+  port.onMessage.addListener(async function(req) {
+    if (req.type === "SUMMARY") {
+      summarize(req, port) 
+    } else if (req.type === "AUTHENTICATE") {
+      authenticate(port) 
+    }
+  })
 });
+
